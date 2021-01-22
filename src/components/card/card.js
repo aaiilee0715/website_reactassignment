199534a3151bff36d555e48573a4000bfee7b051
aaiilee0715 <achansylee@outlook.com>
2021-01-21 21:49:31 -0800

    update pictures

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import Card from './cardUI.jsx';
 
+
+//import images
+import poppyPix from '../images/poppyPix.png';
+import hamsterHotelPix from '../images/hamster.png';
+import multiPix from '../images/multiPix.png';
+import puppyPix from '../images/puppyPix.png';
+
+
 class Cards extends Component {
     render() {
         return (
         <div className="container-fluid d-flex justify-content-center">
         <div className="row">
-        <div className="col-md-4"></div>
-        <Card  />
-        <div className="col-md-4"></div>
-        <Card  />
-        <div className="col-md-4"></div>
-        <Card />
-        <div className="col-md-4"></div>
-        <Card />
+        <div className="col-md-7"></div>
+        <Card imgsrc={poppyPix} />
+        <div className="col-md-3"></div>
+        <Card imgsrc={hamsterHotelPix} />
+        <div className="col-md-3"></div>
+        <Card imgsrc={multiPix}/>
+        <div className="col-md-3"></div>
+        <Card imgsrc={puppyPix}/>
         
          </div>
         </div>
--- src/components/card/cardUI.jsx
+++ src/components/card/cardUI.jsx
@@ -1,22 +1,24 @@
 
 import React from 'react';
-import businessPix from '../images/businessPix.png';
+
 import './card.css';
 
 const Card = props => {
     return (
-        <div className="col-md-4">
+       
             <div className="card text-center">
                 <div className="overflow">
-                    <img src={businessPix} alt="Business Redevolpment"className='card-img-top' />
+                    <img src={props.imgsrc} alt="Image"className='card-img-top' />
                     <div className="card-body text-dark">
                         <h4 className="card-title">Business Redevlopment Website</h4>
-                        <p className="card-text text-secondary">In this team project, we were given a choice to choose a local business and redevelop their website</p>
+                        <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        dipiscing elit pellentesque habitant morbi tristique senectus et netus.</p>
                         <a href="#" className="btn btn-outline-success">Go To Project</a>
                     </div>
                 </div>
             </div>
-        </div>
+      
     );
 }
 export default Card;
